dc3501c7a7e5cf68ad5887346b5a32be7ee4e5e1
Joan Mariñoso Guiu <joanmarinosoguiu@gmail.com>
2022-10-10 19:47:13 +0200

    Exercici_3

---
+++ Sesions_lab/Bloc_1/Sessio_3/Exercici-3/MyGLWidget.cpp
@@ -0,0 +1,176 @@
+
+//#include <GL/glew.h>
+#include "MyGLWidget.h"
+
+#include <iostream>
+
+MyGLWidget::MyGLWidget (QWidget* parent) : QOpenGLWidget(parent), program(NULL)
+{
+  setFocusPolicy(Qt::StrongFocus);  // per rebre events de teclat
+}
+
+MyGLWidget::~MyGLWidget ()
+{
+  if (program != NULL)
+    delete program;
+}
+
+void MyGLWidget::initializeGL ()
+{
+  // Cal inicialitzar l'ús de les funcions d'OpenGL
+  initializeOpenGLFunctions();
+  
+  glClearColor (0.5, 0.7, 1.0, 1.0); // defineix color de fons (d'esborrat)
+  carregaShaders();
+  creaBuffers();
+
+  glUniform1f (sclLoc, scl); //Assigna a un Uniform (sclLoc) d'una dimensió de tipus float el valor de scl
+  // scl variable que conté el valor que es vol per "scl" en el vertex 
+  
+}
+
+void MyGLWidget::paintGL ()
+{
+// En cas de voler canviar els paràmetres del viewport, descomenteu la crida següent i
+// useu els paràmetres que considereu (els que hi ha són els de per defecte)
+//  glViewport (0, 0, ample, alt);
+  
+  glClear (GL_COLOR_BUFFER_BIT);  // Esborrem el frame-buffer
+
+  modelTransform(); //Apliquem TG
+
+  // Activem l'Array a pintar 
+  glBindVertexArray(VAO1);
+ 
+  // Pintem l'escena
+  glDrawArrays(GL_TRIANGLES, 0, 3);
+  
+  // Desactivem el VAO
+  glBindVertexArray(0);
+}
+
+void MyGLWidget::resizeGL (int w, int h)
+{
+// Aquest codi és necessari únicament per a MACs amb pantalla retina.
+#ifdef __APPLE__
+  GLint vp[4];
+  glGetIntegerv (GL_VIEWPORT, vp);
+  ample = vp[2];
+  alt = vp[3];
+#else
+  ample = w;
+  alt = h;
+#endif
+}
+
+void MyGLWidget::creaBuffers ()
+{
+  glm::vec3 Vertices[3];  // Tres vèrtexs amb X, Y i Z
+  Vertices[0] = glm::vec3(-1.0, -1.0, 0.0);
+  Vertices[1] = glm::vec3(1.0, -1.0, 0.0);
+  Vertices[2] = glm::vec3(0.0, 1.0, 0.0);
+  
+
+  glm::vec3 Colors[3];  // Tres vèrtexs amb X, Y i Z
+  Colors[0] = glm::vec3(1.0, 0.0, 0.0);
+  Colors[1] = glm::vec3(0.0, 1.0, 0.0);
+  Colors[2] = glm::vec3(0.0, 0.0, 1.0);
+
+  // Creació del Vertex Array Object (VAO) que usarem per pintar
+  glGenVertexArrays(1, &VAO1);
+  glBindVertexArray(VAO1);
+
+  // Creació del buffer amb les dades dels vèrtexs
+  GLuint VBO1;
+  glGenBuffers(1, &VBO1);
+  glBindBuffer(GL_ARRAY_BUFFER, VBO1);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(Vertices), Vertices, GL_STATIC_DRAW);
+  // Activem l'atribut que farem servir per vèrtex (només el 0 en aquest cas)	
+  glVertexAttribPointer(vertexLoc, 3, GL_FLOAT, GL_FALSE, 0, 0);
+  glEnableVertexAttribArray(vertexLoc);
+
+  GLuint VBO2;
+  glGenBuffers(1, &VBO2);
+  glBindBuffer(GL_ARRAY_BUFFER, VBO2);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(Colors), Colors, GL_STATIC_DRAW);
+  // Activem l'atribut que farem servir per vèrtex (només el 0 en aquest cas)	
+  glVertexAttribPointer(colorLoc, 3, GL_FLOAT, GL_FALSE, 0, 0);
+  glEnableVertexAttribArray(colorLoc);
+
+
+  // Desactivem el VAO
+  glBindVertexArray(0);
+}
+
+void MyGLWidget::carregaShaders()
+{
+  // Creem els shaders per al fragment shader i el vertex shader
+  QOpenGLShader fs (QOpenGLShader::Fragment, this);
+  QOpenGLShader vs (QOpenGLShader::Vertex, this);
+  // Carreguem el codi dels fitxers i els compilem
+  fs.compileSourceFile("shaders/basicShader.frag");
+  vs.compileSourceFile("shaders/basicShader.vert");
+  // Creem el program
+  program = new QOpenGLShaderProgram(this);
+  // Li afegim els shaders corresponents
+  program->addShader(&fs);
+  program->addShader(&vs);
+  // Linkem el program
+  program->link();
+  // Indiquem que aquest és el program que volem usar
+  program->bind();
+
+  // Obtenim identificador per a l'atribut “vertex” del vertex shader
+  vertexLoc = glGetAttribLocation (program->programId(), "vertex");
+  
+  colorLoc = glGetAttribLocation (program->programId(), "color");
+
+  sclLoc = glGetUniformLocation (program->programId(), "scl"); //Uniform
+
+  transLoc = glGetUniformLocation (program->programId(), "TG");
+}
+
+
+void MyGLWidget::keyPressEvent (QKeyEvent *e) {
+  makeCurrent (); //Necessari sempre que es vulgui detectar un events
+  switch ( e->key() ) {  //key() ens indica quina tecla s'ha premut
+    case Qt::Key_S :
+      scl += 0.1;
+      glUniform1f (sclLoc, scl);
+      break;
+    case Qt::Key_D :
+      scl -= 0.1;
+      glUniform1f (sclLoc, scl);
+      break;
+    
+    case Qt::Key_Left:
+      tx -= 0.1;
+      angle += M_PI/4;
+      break;
+    
+    case Qt::Key_Right:
+      tx += 0.1;
+      angle += M_PI/4;
+      break;
+
+    case Qt::Key_Up:
+      ty += 0.1;
+      angle += M_PI/4;
+      break;
+
+    case Qt::Key_Down:
+      ty -= 0.1;
+      angle += M_PI/4;
+      break;  
+    default: e->ignore (); // propagar al pare
+  }
+  update (); //Fen update per a que es torni a dibuixar (GlDraw). Ara ja tindrem el valor de scl serà diferent
+
+}
+
+void MyGLWidget::modelTransform () {  //Igual que exercici 2 pero ara canviant l'ordre de les transformacions
+  glm::mat4 TG (1.0); // Matriu de transformació, inicialment identitat
+  TG = glm::rotate(TG,angle,glm::vec3(0.0,0.0,1.0)); //Rotació
+  TG = glm::translate (TG, glm::vec3 (tx, ty, 0.0)); 
+  glUniformMatrix4fv (transLoc, 1, GL_FALSE, &TG[0][0]);
+}
---
+++ Sesions_lab/Bloc_1/Sessio_3/Exercici-3/MyGLWidget.h
@@ -0,0 +1,51 @@
+#include <QOpenGLFunctions_3_3_Core>
+#include <QOpenGLWidget>
+#include <QOpenGLShader>
+#include <QOpenGLShaderProgram>
+#include "glm/glm.hpp"
+#include "glm/gtc/matrix_transform.hpp" //Incloure funcions transformació geometrica
+#include <QKeyEvent> //Carrega detecció events teclat
+
+class MyGLWidget : public QOpenGLWidget, protected QOpenGLFunctions_3_3_Core
+{
+  Q_OBJECT
+
+  public:
+    MyGLWidget (QWidget *parent=0);
+    ~MyGLWidget ();
+    
+  protected:
+    // initializeGL - Aqui incluim les inicialitzacions del contexte grafic.
+    virtual void initializeGL ();
+
+    // paintGL - Mètode cridat cada cop que cal refrescar la finestra.
+    // Tot el que es dibuixa es dibuixa aqui.
+    virtual void paintGL ();
+ 
+    // resize - Es cridat quan canvia la mida del widget
+    virtual void resizeGL (int width, int height);  
+
+    //Funció per a que passi algo quan apretem una tecla
+    virtual void keyPressEvent ( QKeyEvent * e );
+
+  private:
+    void creaBuffers ();
+    void carregaShaders ();
+    void modelTransform();
+    // attribute locations
+    GLuint vertexLoc;
+    GLuint sclLoc; //Localitzador scl
+    GLuint transLoc; //Localitzador matriu transformació geometrica
+    GLuint colorLoc; //Localitzador color
+    float scl = 0.5f; //Variable on guardarem l'escala actual
+
+    //Variables per fer la translació
+    float tx = 0.0f;
+    float ty = 0.0f;
+    float angle = 0.0f;
+    // Program
+    QOpenGLShaderProgram *program;
+
+    GLuint VAO1;
+    GLint ample, alt;
+};
