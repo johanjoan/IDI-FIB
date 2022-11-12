b305eaf0167a6a06e3c159842afc6e47c3b86c6d
Joan Mariñoso Guiu <joanmarinosoguiu@gmail.com>
2022-11-12 12:59:45 +0100

    Sessio_2_Acabada

---
+++ Sesions_lab/Bloc_2/Sessio_2/Exercici_6/MyGLWidget.h
@@ -0,0 +1,43 @@
+// MyGLWidget.h
+#include "BL2GLWidget.h"
+#include "Model/model.h"
+
+class MyGLWidget : public BL2GLWidget {
+  Q_OBJECT
+
+  public:
+    MyGLWidget(QWidget *parent=0) : BL2GLWidget(parent) {}
+    ~MyGLWidget();
+
+  protected:
+    void creaBuffers();
+    void paintGL();
+    void initializeGL();
+    void viewTransform();
+    void ini_camera();
+    void projectTransform();
+    void carregaShaders();
+    void modelTransformPatricio();
+    void modelTransformTerra();
+    void keyPressEvent (QKeyEvent *event);
+    void parametres_escena(glm::vec3 punt_max,glm::vec3 punt_min);
+    void resizeGL(int w, int h);
+    void calcula_escena();
+
+  private:
+    int printOglError(const char file[], int line, const char func[]);
+    Model m;
+    GLuint VAO1,VAOTerra;
+    GLuint viewLoc;
+    GLuint projLoc;
+    float FOV,FOVini, ra, znear, zfar; //Paràmetres PM
+    glm::vec3 obs,vrp,up; //Paràmetres VM
+    float rotation = 0.0f; //Rotació inicial del Homer
+    glm::vec3 centre_escena;
+    float radi, radi_ample,radi_alt;
+    glm::vec3 punt_max,punt_min;//Punts maxims i mínims de l'escena
+    glm::vec3 base_patricio;
+    float escala_patricio;
+    int opt = 1; //1 == perspectiva; 0 == ortogonal
+};
+
